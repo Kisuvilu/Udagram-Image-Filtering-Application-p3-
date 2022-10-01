6a0a6ea21e3347956efb403b344cd374baa25db0
kisuvilu <kisuvilu@gmail.com>
2022-10-01 19:30:08 +0300

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=kisuvilu
+export POSTGRES_PASSWORD=Dmk.0704970993
+export POSTGRES_HOST=dmk.cgka5ng9tmss.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=my-2022-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
