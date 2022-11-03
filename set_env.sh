865f4a63c9e66ed5e10fd93b5dc322ff3b8dc1d9
kisuvilu <kisuvilu@gmail.com>
2022-11-03 06:01:18 +0300

    photos

--- set_env.sh
+++ set_env.sh
@@ -1,14 +1,14 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=kisuvilu
+export POSTGRES_USERNAME=
 export POSTGRES_PASSWORD=Dmk.0704970993
-export POSTGRES_HOST=kisuvilu.c2tvljks947h.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=database-1.ccalsoledtyf.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=my-2022-dev
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
 export URL=http://localhost:8100
-export AWS_ACCESS_KEY_ID=ASIA5WKXC5RVKTA5AL64
-export AWS_SECRET_ACCESS_KEY=NiRr4cpCNmaSSbQuIlmGTQT4O1D4HEnqJrv467Ej
-export AWS_SESSION_TOKEN=FwoGZXIvYXdzELr//////////wEaDFPYzivdHsB0Nne1sSLVAeWCaPwpUXXlLrBzmFbvcaUGbVhy3t8ZtLRc/a2jfBCBOWt3z+EGquGPCwJu1PbsRfGOxQPA8iZhMxfjXrhwCHNAD/YN4qd+TIrnuJJ4jY9YDiWYvZm2vHk49a0Y2+kgq+Df6Ar2Ck7Y/FMfrEr48ZCqpPBTdl6DC40TF1KbyoKSxrT+igofq8ZZSCzrF0BDvPNIkdTe3BZD4jY4wFsI0i5rqwoanQJLau9Eqpl/Y2Xtgh79MPAqSbxFeejsgCDv2EHoGbpRnEwmSGEI3NnMvQ2x+7BCeSiWkoWbBjIt3AAsYojia0BnmrI9PVEqPl0N3Ed6zJ5+hqyAwU/lZmMmG+5L8++0yswa8s3W
+export AWS_ACCESS_KEY_ID=ASIA5WKXC5RVBKBSQDRL
+export AWS_SECRET_ACCESS_KEY=6SqDoU9nWoxgke4BnxTBekGtnkDaNwRfuE39M/KX
+export AWS_SESSION_TOKEN=FwoGZXIvYXdzENL//////////wEaDCFRM829WsHcIbNfnSLVAdRAEw/Fle661wkexc+2D+PXchRVOn6Ztvx/VA8YIGV4u6FX9h8Ezcuw+dKXStEGi2EjspjKeAHT8Ozom7ABQWbIOECxQU0iMHZ4a2AETveZE+s+ILcWCi67Y6A0kXjA6mmm43ndd3OGIuaF1/d8vWfF80hSoEpRDSmWn055/ZyFJVjbdNylM8dwj5jnAgQ6JdFbupVoaBJN9BPLbO89IYo3gnK1+CDCeBC3lZ+thM/BuiR4Zz2aBCjNz7H0qpjubHt/vMtHWTwSJ45VHoUWCXpyZeLWYyixv4qbBjIt8TfDFk9lfVr+znBqtWNmnfohK+IG3JQyO6PteBHdEzkswDyMWZcXtExdbdv7
